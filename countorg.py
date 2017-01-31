563b1324138db0f0b6a5df2746ec45ffaf9e1f25
zwmtrue <zwmtrue@gmail.com>
2017-01-31 09:38:17 -0500

    Courese Done

---
+++ countorg.py
@@ -0,0 +1,48 @@
+import sqlite3
+import re
+conn = sqlite3.connect('orgcount.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+DROP TABLE IF EXISTS Counts''')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = raw_input('Enter file name: ')
+if ( len(fname) < 1 ) : fname = 'mbox-short.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: ') : continue
+    pieces = line.split()
+    #email = pieces[1]
+    #print pieces[1]
+    orgs = re.findall('@(.+)',pieces[1])
+    #print orgs
+    if len(orgs)<1: continue
+    org=orgs[0]
+    print org
+    #org=orgs[0]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org, ))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES ( ?, 1 )''', ( org, ) )
+    else : 
+        cur.execute('UPDATE Counts SET count=count+1 WHERE org = ?',
+            (org, ))
+    # This statement commits outstanding changes to disk each 
+    # time through the loop - the program can be made faster 
+    # by moving the commit so it runs only after the loop completes
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+print
+print "Counts:"
+for row in cur.execute(sqlstr) :
+    print str(row[0]), row[1]
+
+cur.close()
+
